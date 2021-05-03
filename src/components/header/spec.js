9ee414a969a32879e58af3d7618446dda699d141
Shobana Chakravarthy <shobana.mc@soprasteria.com>
2021-05-03 18:38:20 +0530

    Header Component and tests

--- src/components/header/spec.js
+++ src/components/header/spec.js
@@ -1,8 +1,28 @@
 import React from "react"
 import {shallow} from "enzyme"
 import Header from "./index"
+import {findByTestAttr} from "../../../Utils/index"
 
+const setUp = (props={}) => {
+    const component = shallow(<Header {...props} />)
+    return component
+}
 
-it('It should render without errors',()=>{
-    expect(1).toBe(1)
+describe('Header Component',()=>{
+
+    let component;
+    beforeEach(()=>{
+        component = setUp();
+    })
+
+    it('It should render without errors',()=>{
+       //const wrapper = component.find('.headerComponent');
+       const wrapper = findByTestAttr(component, '.headerComponent')
+       expect(wrapper.length).toBe(1);
+    })
+    it('It should render a logo',()=>{
+        const wrapper = component.find('.logoIMG');
+        expect(wrapper.length).toBe(1);
+     })
 })
+
